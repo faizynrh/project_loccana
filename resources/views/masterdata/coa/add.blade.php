cd573bcf00a1332b7037603256fd7fd47f8ed505
faizynrh <faizyanuarhern@gmail.com>
2025-01-31 14:23:00 +0700

    fix alert blade -faiz

--- resources/views/masterdata/coa/add.blade.php
+++ resources/views/masterdata/coa/add.blade.php
@@ -35,14 +35,6 @@
                             dengan benar.</h6>
                     </div>
                     <div class="card-body">
-                        @if (session('success'))
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                {{ session('success') }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
-                            </div>
-                        @endif
-
                         @if ($errors->any())
                             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                 @foreach ($errors->all() as $error)
