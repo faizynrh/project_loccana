b2409644ca2701387281dc992d6cf2f99c77050e
faizynrh <faizyanuarhern@gmail.com>
2025-02-12 08:43:02 +0700

    fix detail invoice -faiz

--- resources/views/procurement/invoice/detail.blade.php
+++ resources/views/procurement/invoice/detail.blade.php
@@ -51,57 +51,56 @@
                                 <label for="nomorInvoice" class="form-label fw-bold mt-2 mb-1 small">Kode</label>
                                 <input type="text" class="form-control" value="{{ $data->data[0]->id_invoice }}"
                                     readonly>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">Tanggal</label>
                                 <input type="text" class="form-control"
                                     value="{{ \Carbon\Carbon::parse($data->data[0]->order_date)->format('Y-m-d') }}"
                                     readonly>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Principal</label>
-                                <input type="text" class="form-control" readonly>
-                                <label for="shipFrom" class="form-label fw-bold mt-2 mb-1 small">Alamat</label>
-                                <textarea class="form-control" id="shipFrom" rows="4" readonly>{{ $data->data[0]->shipment_info }}</textarea>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Att</label>
-                                <input type="text" class="form-control" readonly>
-                                <label class="form-label fw-bold mt-2 mb-1 small">No. Telp</label>
-                                <input type="text" class="form-control" readonly>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Fax</label>
-                                <input type="text" class="form-control" readonly>
+
+                                <label class="form-label fw-bold mt-2 mb-1 small">Alamat</label>
+                                <textarea class="form-control" rows="4" readonly>{{ $data->data[0]->shipment_info }}</textarea>
                             </div>
+
                             <div class="col-md-6">
                                 <label class="form-label fw-bold mt-2 mb-1 small">Ship To</label>
-                                <textarea class="form-control" id="shipFrom" rows="4" readonly>
+                                <textarea class="form-control" rows="4" readonly>
 JL. Sangkuriang NO.38-A
 NPWP: 01.555.161.7.428.000
-                                </textarea>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Email</label>
-                                <input type="text" class="form-control" readonly>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Telp/Fax</label>
-                                <input type="text" class="form-control" readonly>
+        </textarea>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">VAT/PPN</label>
                                 <input type="text" class="form-control" value="{{ $data->data[0]->ppn }}" readonly>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">Term Pembayaran</label>
                                 <input type="text" class="form-control" value="{{ $data->data[0]->term_of_payment }}"
                                     readonly>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Keterangan</label>
-                                <textarea class="form-control" id="shipFrom" rows="4" readonly></textarea>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">No Invoice</label>
                                 <input type="text" class="form-control" value="{{ $data->data[0]->invoice_number }}"
                                     readonly>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">Tanggal Invoice</label>
                                 <input type="text" class="form-control"
                                     value="{{ \Carbon\Carbon::parse($data->data[0]->invoice_date)->format('Y-m-d') }}"
                                     readonly>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">Tanggal Jatuh Tempo</label>
                                 <input type="text" class="form-control"
                                     value="{{ \Carbon\Carbon::parse($data->data[0]->due_date)->format('Y-m-d') }}" readonly>
+
                                 <label class="form-label fw-bold mt-2 mb-1 small">Keterangan Invoice</label>
-                                <textarea class="form-control" id="shipFrom" rows="4" readonly>{{ $data->data[0]->status }}</textarea>
-                                <label class="form-label fw-bold mt-2 mb-1 small">Faktur Pajak</label>
-                                <input type="text" class="form-control" readonly>
+                                <textarea class="form-control" rows="4" readonly>{{ $data->data[0]->status }}</textarea>
                             </div>
                         </div>
+
                         <div class="p-2">
-                            <h5 class="fw-bold ">Items</h5>
+                            <h5 class="fw-bold">Items</h5>
                         </div>
+
+                        <div class="p-2">
+                            <h5 class="fw-bold">Items</h5>
+                        </div>
+
                         <table class="table mt-3">
                             <thead>
                                 <tr style="border-bottom: 3px solid #000;">
@@ -122,43 +121,41 @@ NPWP: 01.555.161.7.428.000
                                             <p class="fw-bold">{{ $item->qty_on_po }}</p>
                                         </td>
                                         <td>
-                                            <p class="fw-bold"></p>
+                                            <p class="fw-bold">Rp. {{ number_format($item->unit_price, 2, ',', '.') }}</p>
                                         </td>
                                         <td>
-                                            <p class="fw-bold">{{ $item->unit_price }}</p>
+                                            <p class="fw-bold">{{ $item->discount }}%</p>
                                         </td>
-                                        <td>
-                                            <p class="fw-bold">{{ $item->discount }}</p>
-                                        </td>
-                                        <td>
-                                            <p class="fw-bold text-end">{{ $item->total_amount }}</p>
+                                        <td class="text-end">
+                                            <p class="fw-bold">Rp. {{ number_format($item->total_po, 2, ',', '.') }}</p>
                                         </td>
                                     </tr>
                                 @endforeach
+
                                 <tr class="fw-bold">
-                                    <td colspan="4"></td>
-                                    <td>Sub Total</td>
-                                    <td style="float: right;">{{ $data->data[0]->total_price }}</td>
+                                    <td colspan="4" class="text-end">Sub Total</td>
+                                    <td class="text-end">Rp. {{ number_format($data->data[0]->total_price, 2, ',', '.') }}
+                                    </td>
                                 </tr>
                                 <tr class="fw-bold">
-                                    <td colspan="4"></td>
-                                    <td>Diskon</td>
-                                    <td style="float: right;">{{ $data->data[0]->discount_invoice }}</td>
-                                </tr class="fw-bold">
+                                    <td colspan="4" class="text-end">Diskon</td>
+                                    <td class="text-end">Rp.
+                                        {{ number_format($data->data[0]->discount_invoice, 2, ',', '.') }}</td>
+                                </tr>
                                 <tr class="fw-bold">
-                                    <td colspan="4"></td>
-                                    <td>Taxable</td>
-                                    <td style="float: right">{{ $data->data[0]->total_amount }}</td>
-                                </tr class="fw-bold">
+                                    <td colspan="4" class="text-end">Taxable</td>
+                                    <td class="text-end">Rp. {{ number_format($data->data[0]->total_amount, 2, ',', '.') }}
+                                    </td>
+                                </tr>
                                 <tr class="fw-bold">
-                                    <td colspan="4"></td>
-                                    <td>VAT/PPN</td>
-                                    <td style="float: right">{{ $data->data[0]->ppn }}</td>
+                                    <td colspan="4" class="text-end">VAT/PPN</td>
+                                    <td class="text-end">0
+                                    </td>
                                 </tr>
                                 <tr class="fw-bold" style="border-top: 2px solid #000">
-                                    <td colspan="4"></td>
-                                    <td>Total</td>
-                                    <td style="float: right">{{ $data->data[0]->total_po }}</td>
+                                    <td colspan="4" class="text-end">Total</td>
+                                    <td class="text-end">Rp.
+                                        {{ number_format($data->data[0]->total_amount, 2, ',', '.') }}</td>
                                 </tr>
                             </tbody>
                         </table>
