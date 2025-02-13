69762926b3c4d7c68fd5411691e437c115472b51
faizynrh <faizyanuarhern@gmail.com>
2025-02-13 14:16:14 +0700

    ubah judul halaman -faiz

--- resources/views/procurement/return/add.blade.php
+++ resources/views/procurement/return/add.blade.php
@@ -10,7 +10,7 @@
             <div class="page-title">
                 <div class="row">
                     <div class="col-12 col-md-6 order-md-1 order-last">
-                        <h3>Tambah Invoice</h3>
+                        <h3>Tambah Return Pembelian</h3>
                     </div>
                     <div class="col-12 col-md-6 order-md-2 order-first">
                         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
@@ -19,7 +19,7 @@
                                     <a href="index.html">Dashboard</a>
                                 </li>
                                 <li class="breadcrumb-item active" aria-current="page">
-                                    Tambah Invoice
+                                    Tambah Return Pembelian
                                 </li>
                             </ol>
                         </nav>
