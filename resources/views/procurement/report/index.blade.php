9b266df1deb78570f284154501bdcc72bd6accb8
faizynrh <faizyanuarhern@gmail.com>
2025-02-14 14:09:44 +0700

    ubah tampilan judul -faiz

--- resources/views/procurement/report/index.blade.php
+++ resources/views/procurement/report/index.blade.php
@@ -9,7 +9,7 @@
             <div class="page-title">
                 <div class="row">
                     <div class="col-12 col-md-6 order-md-1 order-last">
-                        <h3>Laporan Dasar Pembelian</h3>
+                        <h3>Laporan Pembelian</h3>
                     </div>
                     <div class="col-12 col-md-6 order-md-2 order-first">
                         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
@@ -18,7 +18,7 @@
                                     <a href="/dashboard">Dashboard</a>
                                 </li>
                                 <li class="breadcrumb-item active" aria-current="page">
-                                    Laporan Dasar Pembelian
+                                    Laporan Pembelian
                                 </li>
                             </ol>
                         </nav>
