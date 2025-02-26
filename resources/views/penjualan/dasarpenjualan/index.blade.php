d869dfbb5f073ff85330d673c77f4adfc6694ec3
faizynrh <faizyanuarhern@gmail.com>
2025-02-26 11:19:50 +0700

    fix layout dasar penjualan -faiz

--- resources/views/penjualan/dasarpenjualan/index.blade.php
+++ resources/views/penjualan/dasarpenjualan/index.blade.php
@@ -2,6 +2,21 @@
 @section('content')
     @push('styles')
         <style>
+            /* #tabledasarpenjualan thead tr:first-child th {
+                        position: sticky;
+                        background: white;
+                        z-index: 0;
+                        border-bottom: 2px solid #ddd;
+                    }
+
+                    #tabledasarpenjualan thead tr:first-child th {
+                        top: 0;
+                    }
+
+                    .table-responsive {
+                        max-height: 50px;
+                        overflow-y: auto;
+                    } */
         </style>
     @endpush
     <div id="main-content">
