f83e1c5b60dfaf9a84abc7a11b8c8de433d186b7
faizynrh <faizyanuarhern@gmail.com>
2025-03-14 14:36:22 +0700

    perbaikan form list select -faiz

--- resources/views/masterdata/principal/ajax/add.blade.php
+++ resources/views/masterdata/principal/ajax/add.blade.php
@@ -5,9 +5,8 @@
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="kode" class="form-label fw-bold">Tipe Partner</label>
-                    <select type="number" name="partner_type_id" placeholder="Type Partner" class="form-select"
-                        id="partner_type_id"
-                        style="pointer-events: none;background-color: #f0f0f0;color: #888;cursor: not-allowed;">
+                    <select type="number" name="partner_type_id" placeholder="Type Partner"
+                        class="form-select bg-body-secondary" id="partner_type_id" style="pointer-events: none">
                         <option value="" disabled selected>Pilih Tipe</option>
                         @if (isset($partner->data))
                             @foreach ($partner->data as $partner)
